406de1472c12a8556d48748274ba34aaaa6738dc
Asma <asma.ansari.na@gmail.com>
2024-02-13 20:53:29 +0500

    bootstrap changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.css';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import Navbar from './navbar'
 import Home from './pages/home'
