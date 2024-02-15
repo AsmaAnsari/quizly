f5c529900584df5cfee5fd4fc49e5350e1166bb7
Asma <asma.ansari.na@gmail.com>
2024-02-15 16:41:21 +0500

    style change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./style.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
