eb4edc2c59d6c6b8e7e04112d98f2860369c8306
Asma <asma.ansari.na@gmail.com>
2024-03-03 09:51:32 +0500

    index file changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import "./style.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.css';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import Navbar from './navbar'
 import Home from './pages/home'
 import About from './pages/about'
@@ -14,6 +16,8 @@ import Math from './pages/math';
 import Economics from './pages/economics';
 import WebQuiz from './pages/webQuiz';
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 const router = createBrowserRouter([
   {
@@ -54,9 +58,7 @@ const router = createBrowserRouter([
   },
 
 ]);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router}/>
+  
 );
